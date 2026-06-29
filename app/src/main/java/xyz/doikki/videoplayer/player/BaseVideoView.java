d2e30031135cbbf305274651537583a337859bac
zhaoyuheqing <proooprop@outlook.com>
2026-06-29 13:54:10 +0800

    Update BaseVideoView.java

--- app/src/main/java/xyz/doikki/videoplayer/player/BaseVideoView.java
+++ app/src/main/java/xyz/doikki/videoplayer/player/BaseVideoView.java
@@ -412,8 +412,10 @@ public class BaseVideoView<P extends AbstractPlayer> extends FrameLayout
      * 保存播放进度
      */
     protected void saveProgress() {
+        getCurrentPosition()
         if (mProgressManager != null && mCurrentPosition > 0) {
             L.d("saveProgress: " + mCurrentPosition);
+                getCurrentPosition()
             mProgressManager.saveProgress(mProgressKey == null ? mUrl : mProgressKey, mCurrentPosition);
         }
     }
